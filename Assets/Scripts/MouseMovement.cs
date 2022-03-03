d05e50b3a4c40fa1a54561cb2726f312878d482b
Suhaib <suhaib1602@gmail.com>
2022-03-03 19:19:27 +0000

    Implemented Player Movement
    Physics + Mouse Movement.

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+
+    [Header("Mouse Rotation Components")]
+    [Range(-360,0)]
+    public float rotateYMin = -90f;
+    [Range(0,360)]
+    public float rotateYMax = 90f;
+    [Range(0,360)]
+    public float rotateXSpeed = 90f;
+    [Range(0,360)]
+    public float rotateYSpeed = 90f;
+
+    [SerializeField] private float xRotation;
+    [SerializeField] private float yRotation;
+
+    void Start(){
+        cam = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        configureRotation();
+        
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    void configureRotation(){
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        //Not inverse Rotation
+        xRotation -= mouseY * rotateXSpeed * Time.deltaTime;
+        yRotation += mouseX * rotateYSpeed * Time.deltaTime;
+
+        xRotation = Mathf.Clamp(xRotation, rotateYMin, rotateYMax);
+        
+    }
+}
