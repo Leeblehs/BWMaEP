b0da03789a3ab286b20c2b8bd764a69f345fe324
Leebleh <leeblehs@gmail.com>
2022-03-19 23:13:52 +0000

    Radio Bug Fix
    Forgot to stop particles when paused, previously only when stopped!

--- Assets/RadioController.cs
+++ Assets/RadioController.cs
@@ -26,6 +26,7 @@ public class RadioController : MonoBehaviour
              if (Input.GetKeyDown(KeyCode.F2)) 
             {
                 PauseAudio();
+                musicalNotes.Stop();
             }
 
              if (Input.GetKeyDown(KeyCode.F3)) 
